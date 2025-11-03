6d8c82c8b2cc6ad61b06aa66137d853556d3d8a1
shubhangi2025 <shubha.patil8458@gmail.com>
2025-11-03 10:41:30 +0100

    added get all products functionality

--- src/main/java/com/ecommerce/project/sb_ecom/com/ecommerce/project/payload/ProductResponse.java
+++ src/main/java/com/ecommerce/project/sb_ecom/com/ecommerce/project/payload/ProductResponse.java
@@ -1,7 +1,11 @@
 package com.ecommerce.project.sb_ecom.com.ecommerce.project.payload;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter @Setter
 public class ProductResponse {
     private List<ProductDTO> content;
 }
--- src/main/java/com/ecommerce/project/sb_ecom/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/sb_ecom/com/ecommerce/project/service/ProductServiceImpl.java
@@ -4,11 +4,14 @@ import com.ecommerce.project.sb_ecom.com.ecommerce.project.exception.ResourceNot
 import com.ecommerce.project.sb_ecom.com.ecommerce.project.model.Category;
 import com.ecommerce.project.sb_ecom.com.ecommerce.project.model.Product;
 import com.ecommerce.project.sb_ecom.com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.sb_ecom.com.ecommerce.project.payload.ProductResponse;
 import com.ecommerce.project.sb_ecom.com.ecommerce.project.repositories.CategoryRepository;
 import com.ecommerce.project.sb_ecom.com.ecommerce.project.repositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService{
@@ -26,12 +29,23 @@ public class ProductServiceImpl implements ProductService{
     public ProductDTO addProduct(Long categoryId, Product product) {
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow( ()-> new ResourceNotFoundException("Category", "categoryId",categoryId));
-
+        product.setProductName(product.getProductName());
         product.setImage("default.png");
         product.setCategory(category);
         double specialPrice = product.getPrice() - ((product.getDiscount() * 0.01) * product.getPrice());
         Product savedProduct = productRepository.save(product);
         ProductDTO mappedProduct = modelMapper.map(product, ProductDTO.class);
         return mappedProduct;
+    }
+
+    @Override
+    public ProductResponse getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        List<ProductDTO> productDTOlist = products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .toList();
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productDTOlist);
+        return productResponse;
     }
 }
